a02244d315bf7824e53aa04ae828fa5caa393e45
madnessguy67 <michaelkaliber15@gmail.com>
2021-10-11 21:42:45 +0100

    Encrypt and decrypt using key

--- Encryption-Decryption/Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -5,25 +5,35 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String operation = scanner.nextLine();
         char[] string = scanner.nextLine().toCharArray(); //abc vwx = fgh abc
         int key = scanner.nextInt();
 
-        String output = "";
-
-        for (int i = 0; i < string.length; i++) {
-            if (string[i] == ' ') {
-                output += string[i];
-                continue;
-            }
-            char toAdd = (char) ((char) string[i] + key);
-
-            if (string[i] + key > 'z') {
-                output = output + (char) ((char) toAdd - 26); //read the integer value of i + 5 and subtract length of alphabet
-            } else {
-                output += toAdd;
-            }
+        if (operation.equals("enc")) {
+            enc(string, key);
+        } else {
+            dec(string, key);
         }
 
-        System.out.println(output);
+    }
+
+    public static void enc(char[] arr, int key) {
+        char[] chars = arr.clone();
+        StringBuilder string = new StringBuilder();
+
+        for (char aChar : chars) {
+            string.append((char) (aChar + key));
+        }
+        System.out.println(string);
+    }
+
+    public static void dec(char[] arr, int key) {
+        char[] chars = arr.clone();
+        StringBuilder string = new StringBuilder();
+
+        for (char aChar : chars) {
+            string.append((char) (aChar - key));
+        }
+        System.out.println(string);
     }
 }
