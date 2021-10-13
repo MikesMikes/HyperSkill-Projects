7bedcef06784339cd08fe067bfe03e3b203f8d86
madnessguy67 <michaelkaliber15@gmail.com>
2021-10-13 17:57:24 +0100

    Command-line args array exercise

---
+++ Encryption-Decryption/Topics/Command-line arguments/The index of an argument/src/Main.java
@@ -0,0 +1,9 @@
+import java.sql.Array;
+import java.util.Arrays;
+
+class Problem {
+    public static void main(String[] args) {
+        final String key = "test";
+        System.out.println(Arrays.asList(args).indexOf(key));
+    }
+}
