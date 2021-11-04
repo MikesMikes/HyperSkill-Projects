bf7c18c37fc4a1b8362eb2e1bf74f990ceb556de
madnessguy67 <michaelkaliber15@gmail.com>
2021-11-04 20:17:23 +0000

    Practice

---
+++ Scratch_Files/src/main/java/Scratch.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Scratch {
+    public static void main(String[] args) {
+        String currentDir = System.getProperty("user.dir");
+        System.out.println(currentDir);
+    }
+}
